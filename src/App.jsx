80cf48731c6574be527b1599650b5b99b1eb29ba
GauthamS-013 <gauthams779@gmail.com>
2025-01-24 14:38:01 +0530

    'first'

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+import Add from "./components/Add";
+import List from "./components/List";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [success,setSuccess]=useState("")
+
+  return (
+    <>
+      <nav className="navbar navbar-expand navbar-light bg-danger" style={{minWidth:'100vh'}}>
+        <a className="navbar-brand ms-4" href="">
+          {' '}
+        <i className="fa-solid fa-film text-light me-2"></i>
+         <b className="text-light"> MOVIE WATCHLIST</b>
+        </a>
+      </nav>
+
+      <div className="container-fluid" style={{backgroundColor:'#e6dda3',minHeight:'100vh',minWidth:'100vh'}}>
+        <Add val={setSuccess}/>
+        <List success={success}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import Edit from "./Edit";
+import { getMovieApi,deleteMovieApi } from "../services/allApis";
+
+function List({success}) {
+  const [movieList, setMovieList] = useState([]);
+  const [editRes,setEditRes]=useState("")
+
+  useEffect(() => {
+    getData();
+  }, [success,editRes]);
+
+  const getData = async () => {
+    const result = await getMovieApi();
+    console.log(result);
+    if (result.status === 200) {
+      setMovieList(result.data);
+    }
+  };
+
+  const deleteMovie = async (id)=>{
+    const result=await deleteMovieApi(id)
+    console.log(result)
+    if(result.status===200){
+      getData()
+    }
+    else{
+      alert("Something Went Wrong !!")
+    }
+  }
+
+  return (
+    <>
+      {movieList.length > 0 ? (
+        
+          <table className="table table-bordered border-5 shadow text-dark mx-2 border-dark">
+            <thead className="thead-dark">
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Year</th>
+                <th>Poster</th>
+                <th>...</th>
+              </tr>
+            </thead>
+            <tbody>
+              {movieList.map((item) => (
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.year}</td>
+                  <td>
+                    <img
+                      src={item.image}
+                      height="200px"
+                      
+                      alt="Thudarum poster"
+                    />
+                  </td>
+                  <td>
+                    <div>
+                      <Edit movie={item} edit={setEditRes}/>
+                      <button className="btn btn-danger" onClick={()=>{deleteMovie(item.id)}}>Delete</button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        
+      ) : (
+        <h3 className="my-5 text-center text-light">
+          No Movies Added Yet !!
+        </h3>
+      )}
+    </>
+  );
+}
+
+export default List;
